b22d6fbbc8cfe2fcc5a7c7d932cbe0ac38ff23e2
Jordan <36747938+Jordantayl@users.noreply.github.com>
2018-11-14 23:00:15 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -302,7 +302,25 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 break;
             //!music - plays a tune FIXME
             case 'music':
-                bot.joinVoiceChannel('General')
+            client.joinVoiceChannel(voiceChannelID, function(error, events) {
+                //Check to see if any errors happen while joining.
+                if (error) return console.error(error);
+              
+                //Then get the audio context
+                client.getAudioContext(voiceChannelID, function(error, stream) {
+                  //Once again, check to see if any errors exist
+                  if (error) return console.error(error);
+              
+                  //Create a stream to your file and pipe it to the stream
+                  //Without {end: false}, it would close up the stream, so make sure to include that.
+                  fs.createReadStream('myFile.mp3').pipe(stream, {end: false});
+              
+                  //The stream fires `done` when it's got nothing else to send to Discord.
+                  stream.on('done', function() {
+                     //Handle
+                  });
+                });
+              });
                 break;
             case 'bitcoin':
                 bitcoin.getBitcoinPrice(function (price) {
